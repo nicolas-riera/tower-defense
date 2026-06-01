7ceaa80462b73ac2f0e2899bfe72aa2c87105f9e
Arthur GEORGET <arthur.georget@laplateforme.io>
2026-06-01 10:18:32 +0200

    feat(struct): created folder and file structure.
    Co-authored-by: Nicolas <nicolas-riera@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "raylib.h"
 
 int main(void)
@@ -9,30 +10,14 @@ int main(void)
     SetConfigFlags(FLAG_VSYNC_HINT); // Enable VSYNC
     InitWindow(screenWidth, screenHeight, "Tower Defense");
 
-    SetTargetFPS(60);   
-    
-    Vector2 squarePosition = { (float)screenWidth / 2 - 25, (float)screenHeight / 2 - 25 };
-    const float speed = 5.0f;
+    SetTargetFPS(60); 
+
+
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        if (IsKeyDown(KEY_RIGHT)) squarePosition.x += speed;
-        if (IsKeyDown(KEY_LEFT)) squarePosition.x -= speed;
-        if (IsKeyDown(KEY_UP)) squarePosition.y -= speed;
-        if (IsKeyDown(KEY_DOWN)) squarePosition.y += speed;
-
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            DrawRectangleV(squarePosition, (Vector2){ 50.0f, 50.0f }, MAROON);
-
-            DrawFPS(10, 30);
-
-            DrawText("Use arrows to move the cube", 10, 10, 20, DARKGRAY);
-
-        EndDrawing();
+        
     }
 
     CloseWindow();        
