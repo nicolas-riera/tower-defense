cc278634663b37d5fbfdf0c8dbdbba158edee344
nicolas-riera <habbo.rieranicolas08@gmail.com>
2026-06-02 10:34:26 +0200

    feat: added label to option menu

--- src/view/menu/StateOptionsView.cpp
+++ src/view/menu/StateOptionsView.cpp
@@ -11,6 +11,8 @@ void StateOptionsView::init(){
 void StateOptionsView::display(const std::vector<std::unique_ptr<Button>>& buttons){
     DrawTextureEx(this->background, { 0.0f, 0.0f }, 0, 1, WHITE);
 
+    DrawText("OPTIONS", (this->context->screenWidth / 2) - 72, 170, 32, BLACK);
+
     for (auto& btn : buttons) {
         btn->Draw();
     };
