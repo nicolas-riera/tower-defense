efa76604f09ab8b41d0a0d32a7e19bbb2e9300da
nicolas-riera <habbo.rieranicolas08@gmail.com>
2026-06-04 16:24:00 +0200

    feat: finalized pause menu

--- src/controller/game/StateGamePauseMenu.cpp
+++ src/controller/game/StateGamePauseMenu.cpp
@@ -8,6 +8,18 @@ void StateGamePauseMenu::init(){
     this->view = std::make_unique<StateGamePauseMenuView>();
     this->view->init();
     this->view->setContext(this->context);
+
+    this->gamePauseMenuButtons.push_back(std::make_unique<Button>((this->context->getContext()->screenWidth / 2) - 75, 350, 150, 50, "Resume", [this]() {
+        this->context->setState(RUNNING);
+    }, this->context->getContext()));
+
+    this->gamePauseMenuButtons.push_back(std::make_unique<Button>((this->context->getContext()->screenWidth / 2) - 75, 410, 150, 50, "Restart", [this]() {
+        this->context->getContext()->setState(GAME); 
+    }, this->context->getContext()));
+
+    this->gamePauseMenuButtons.push_back(std::make_unique<Button>((this->context->getContext()->screenWidth / 2) - 75, 470, 150, 50, "Quit", [this]() {
+        this->context->getContext()->setState(MENU);
+    }, this->context->getContext()));
 };
 
 void StateGamePauseMenu::expose(){
